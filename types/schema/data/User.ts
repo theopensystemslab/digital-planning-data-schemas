96b91748d57fd55d411c91ba9381ceafcbf7bf55
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-24 11:02:20 +0100

    demo: Example of conditional types

--- types/schema/data/User.ts
+++ types/schema/data/User.ts
@@ -1,3 +1,32 @@
-export interface User {
+/**
+ * @id User
+ * @description The user who completed the service. They could have completed it for themselves, or on behalf of another.
+ */
+export type User = ApplicantUser | AgentUser;
+
+export type Role = 'applicant' | 'agent';
+
+export interface BasePerson<TRole extends Role> {
   name: string;
+  role: TRole;
 }
+
+export interface ApplicantUser extends BasePerson<'applicant'> {
+  applicantAddress: string[];
+}
+
+export interface AgentUser extends BasePerson<'agent'> {
+  agentNumber: number;
+}
+
+export const testApplicant: ApplicantUser = {
+  name: 'daf',
+  role: 'applicant',
+  applicantAddress: [],
+};
+
+export const testAgent: AgentUser = {
+  name: 'daf',
+  role: 'agent',
+  agentNumber: 123,
+};
