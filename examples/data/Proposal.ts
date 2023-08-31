ea0d61b1f0b4c0164658217ab3c1cfea576e75f9
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-08-31 22:27:11 +0200

    support one or many project types

---
+++ examples/data/Proposal.ts
@@ -0,0 +1,14 @@
+import {Schema} from '../../types/Schema';
+
+export const validProposal: Schema['data']['proposal'] = {
+  projectType: [
+    {
+      value: 'alter.equipment.solar',
+      description: 'Install solar panels',
+    },
+    {
+      value: 'new.leisure',
+      description: 'Leisure premises',
+    },
+  ],
+};
--- types/schema/data/Proposal.ts
+++ types/schema/data/Proposal.ts
@@ -1,3 +1,22 @@
+import {ProjectTypes} from '../../enums/ProjectTypes';
+
 export interface Proposal {
-  name: string;
+  projectType: ProjectType[];
 }
+
+type ProjectTypeKeys = keyof typeof ProjectTypes;
+
+type GenericProjectType<TKey extends ProjectTypeKeys> = {
+  value: TKey;
+  description: (typeof ProjectTypes)[TKey];
+};
+
+type ProjectTypeMap = {
+  [K in ProjectTypeKeys]: GenericProjectType<K>;
+};
+
+/**
+ * @id #ProjectType
+ * @description Planning project types
+ */
+export type ProjectType = ProjectTypeMap[keyof ProjectTypeMap];
