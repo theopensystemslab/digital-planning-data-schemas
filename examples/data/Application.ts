00368a381128b46c5c44de674a1e1465cd886ed6
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-08-31 13:37:29 +0200

    feat: `Application` type (#25)

---
+++ examples/data/Application.ts
@@ -0,0 +1,28 @@
+import {Schema} from '../../types/Schema';
+
+export const validApplication: Schema['data']['application'] = {
+  type: {
+    value: 'ldc.proposed',
+    description: 'Lawful Development Certificate - Proposed',
+  },
+  fee: {
+    calculated: 103,
+    payable: 103,
+    exemption: {
+      disability: false,
+      resubmission: false,
+    },
+    reduction: {
+      alternative: false,
+      parishCouncil: false,
+      sports: false,
+    },
+    reference: {
+      govPay: 'abc123',
+    },
+  },
+  declaration: {
+    accurate: true,
+    connection: 'none',
+  },
+};
