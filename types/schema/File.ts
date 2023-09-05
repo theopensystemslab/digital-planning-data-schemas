4bde898d536a72bb29805cfc801503f5c24e22d2
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-09-05 15:38:04 +0200

    feat: `File` type (#27)

--- types/schema/File.ts
+++ types/schema/File.ts
@@ -1,7 +1,28 @@
+import {FileTypes} from '../enums/FileTypes';
+
 /**
- * @example { size: "10mb" }
+ * @id #File
+ * @description Files uploaded by the user to support this application
  */
 export interface File {
-  size: string;
-  foo: 'bar' | 'baz' | 'boo';
+  name: string;
+  type: FileType[];
+  description?: string;
 }
+
+type FileTypeKeys = keyof typeof FileTypes;
+
+type GenericFileType<TKey extends FileTypeKeys> = {
+  value: TKey;
+  description: (typeof FileTypes)[TKey];
+};
+
+type FileTypeMap = {
+  [K in FileTypeKeys]: GenericFileType<K>;
+};
+
+/**
+ * @id #FileType
+ * @description Types of planning documents and drawings
+ */
+export type FileType = FileTypeMap[keyof FileTypeMap];
