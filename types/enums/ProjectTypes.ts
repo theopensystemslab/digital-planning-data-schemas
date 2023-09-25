c990a8298f1416492aa51344dfe0dc4fe1b31c01
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-09-25 14:13:20 +0200

    more granular projectTypes

--- types/enums/ProjectTypes.ts
+++ types/enums/ProjectTypes.ts
@@ -4,9 +4,13 @@
 export const ProjectTypes = {
   'alter.balcony': 'Add or alter a balcony',
   'alter.bayWindow': 'Add or remove a bay window',
+  'alter.bayWindow.add': "Add a bay window",
+  'alter.bayWindow.remove': "Remove a bay window",
   'alter.boundary': 'Changes to a fence, wall or gate',
   'alter.cables': 'Install underground cables',
   'alter.chimneys': 'Change chimneys',
+  'alter.chimneys.add': "Add a chimney",
+  'alter.chimneys.replace': "Replace a chimney",
   'alter.drains': 'Work on drains',
   'alter.equipment.airConditioning': 'Install an air conditioning unit',
   'alter.equipment.alarm': 'Install a security alarm',
@@ -16,21 +20,34 @@ export const ProjectTypes = {
   'alter.equipment.charging': 'Install a car charging point',
   'alter.equipment.heatPump': 'Install a heat pump',
   'alter.equipment.lighting': 'Install outdoor lights',
-  'alter.equipment.solar': 'Install solar panels',
+  'alter.equipment.solar.pv': "Install solar photovoltaics (PV)",
+  'alter.equipment.solar.thermal': "Install solar thermal equipment",
   'alter.equipment.tank': 'Install an outdoor tank (for example a water tank)',
   'alter.equipment.wind': 'Install a wind turbine',
   'alter.facades': 'Change the material or colour of the external walls',
-  'alter.highways':
-    'Changes to a public road, pavement or path (including drop kerb)',
+  'alter.highways': 'Changes to a public road, pavement or path (including drop kerb)',
   'alter.landscape': 'Landscaping works',
   'alter.landscape.ponds': 'Add or remove a pond',
-  'alter.openings.add': 'Add new doorways or windows',
+  'alter.openings.add.door': "Add one or more new doorways",
+  'alter.openings.add.window': "Add one or more new windows",
+  'alter.openings.add.windows.high': 'Add doorways or new windows - 1.7m or higher',
   'alter.openings.alter': 'Change the size of doorways or windows',
+  'alter.openings.alter.convert.doorToWindow': "Convert a doorway into a window",
+  'alter.openings.alter.convert.windowToDoor': "Convert a window into a doorway",
+  'alter.openings.alter.enlarge.door': "Enlarge a door opening",
+  'alter.openings.alter.enlarge.window': "Enlarge a window opening",
+  'alter.openings.alter.reduce.door': "Reduce the size of a door opening",
+  'alter.openings.alter.reduce.window': "Reduce the size of a window opening",
   'alter.openings.remove': 'Block up doorways or windows',
   'alter.remove': 'Remove part of a building (such as a decorative feature)',
+  'alter.remove.chimney': 'Remove a chimney', // Propose: alter.chimeys.remove ?? 
   'alter.remove.equipment': 'Remove equipment', // Remove energy equipment
   'alter.repair': 'Repair windows or doors',
   'alter.replace': 'Replace windows or doors',
+  'alter.replace.doorsToDoors': 'Replace door with door',
+  'alter.replace.doorsToWindows': 'Replace door with window', 
+  'alter.replace.windowsToDoors': 'Replace window with door',
+  'alter.replace.windowsToWindows': 'Replace window with window',
   'alter.roof.materials': 'Replace or change the roof materials',
   'alter.roof.roofTerrace': 'Add a roof terrace',
   'alter.roof.shape': 'Change the shape of a roof',
@@ -44,20 +61,18 @@ export const ProjectTypes = {
   'alter.surfaces.parking': 'Add a driveway or parking area',
   'alter.swimmingPool': 'Install a swimming pool',
   'alter.trees': 'Changes to trees or hedges',
-  changeOfUse: 'Convert a building to a different use',
-  'changeofUse.annexe':
-    'Convert part of the property into a  granny flat (residential annexe)',
+  'changeOfUse': 'Convert a building to a different use',
+  'changeofUse.annexe': 'Convert part of the property into a  granny flat (residential annexe)',
   'changeOfUse.caravans': 'Use a caravan or mobile home on the property',
   'changeOfUse.garage': 'Convert a garage',
   'changeOfUse.let.part': 'Let a part of the property',
   'changeOfUse.let.whole': 'Let the property',
-  'changeOfUse.outbuilding':
-    'Convert an outbuilding (such as a shed, garage or barn)', // Change the use of an outbuilding (such as a shed, garage or barn)
+  'changeOfUse.outbuilding': 'Convert an outbuilding (such as a shed, garage or barn)', // Change the use of an outbuilding (such as a shed, garage or barn)
   'changeOfUse.part': 'Convert part of a building', // Change the use of part of a building
-  'changeOfUse.whole': 'Change the use of a property',
   'changeOfUse.whole.homeToHMO': 'Convert a home to bedsits or a shared home', // Share a home
+  'changeOfUse.whole': 'Change the use of a property',
   'changeOfUse.workFromHome': 'Work from home',
-  demolish: 'Demolish a building',
+  'demolish': 'Demolish a building',
   'demolish.boundary': 'Demolish a fence, gate or boundary wall',
   'demolish.full': 'Demolish a building',
   'demolish.outbuildings': 'Demolish an outbuilding (such as a garage or barn)',
@@ -65,27 +80,25 @@ export const ProjectTypes = {
   'demolish.replace': 'Demolish a building and build homes in its place',
   'extend.basement': 'Add a basement extension',
   'extend.front': 'Add a front extension',
-  'extend.outbuildings':
-    'Add an outbuilding (such as a shed, garage or garden office)', // Outbuildings (shed, car shelter or garden office)
+  'extend.outbuildings': 'Add an outbuilding (such as a shed, garage or garden office)', // Outbuildings (shed, car shelter or garden office)
   'extend.porch': 'Add a porch',
   'extend.rear': 'Add a rear or side extension (or conservatory)',
   'extend.roof': 'Add a roof extension',
   'extend.roof.dormer': 'Add roof dormers',
-  internal: 'Change the internal layout', // Internal building works
+  'internal': 'Change the internal layout', // Internal building works
   'internal.loft': 'Convert a loft',
   'internal.mezzanine': 'Add a mezzanine floor',
-  new: 'Another type of building',
+  'new': 'Another type of building',
   'new.agriculture': 'Agricultural buildings',
   'new.clickCollect': 'Install click and collect facilities',
   'new.forestry': 'Build new forestry buildings',
-  'new.leisure': 'Leisure premises',
   'new.industrial': 'Industrial premises',
+  'new.leisure': 'Leisure premises',
   'new.office': 'Offices',
   'new.residential.dwelling': 'Build new homes on a roof', // Homes
   'new.retail': 'Retail premises',
   'new.telecoms': 'Install telecommunications equipment',
-  'new.temporaryStructure':
-    'Erect a temporary structure for historic visitor attractions and listed buildings',
+  'new.temporaryStructure': 'Erect a temporary structure for historic visitor attractions and listed buildings',
   'new.warehouse': 'Storage or distribution premises',
   'unit.merge': 'Convert two or more properties into one',
   'unit.subdivide': 'Convert part of a house into a flat', // Convert a home into flats
