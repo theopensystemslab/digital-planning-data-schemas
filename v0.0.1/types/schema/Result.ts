c867bd57d13ccc7b4d1116d146d58d0b7c8f7f57
jessicamcinchak <jessicamcinchak@users.noreply.github.com>
2023-09-29 12:28:31 +0000

    Add build files for v0.0.1

---
+++ v0.0.1/types/schema/Result.ts
@@ -0,0 +1,24 @@
+import {Flags} from '../enums/Flags';
+
+/**
+ * @id #Result
+ * @description The result of this application. Results are determined by flags corresponding to responses; each application can have up to one result per flagset
+ */
+export type Result = ResultFlag[]; // @todo validate/restrict array to one result per flagset
+
+type FlagKeys = keyof typeof Flags;
+
+type GenericFlag<TKey extends FlagKeys> = {
+  value: TKey;
+  description: (typeof Flags)[TKey];
+};
+
+type FlagMap = {
+  [K in FlagKeys]: GenericFlag<K>;
+};
+
+/**
+ * @id #ResultFlag
+ * @description An individual result of this application
+ */
+export type ResultFlag = FlagMap[keyof FlagMap];
