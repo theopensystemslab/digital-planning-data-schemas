394c86a5a55449c1a461cca2186890cc1b4c7a56
jessicamcinchak <jessicamcinchak@users.noreply.github.com>
2024-01-29 16:50:30 +0000

    Add build files for v0.2.3

---
+++ v0.2.3/types/enums/PlanningConstraints.ts
@@ -0,0 +1,30 @@
+/**
+ * Values for `data.property.planning.designations`
+ */
+export const PlanningDesignations = {
+  article4: 'Article 4 Direction area',
+  'article4.caz': 'Central Activities Zone (CAZ)',
+  brownfieldSite: 'Brownfield site',
+  designated: 'Designated land',
+  'designated.AONB': 'Area of Outstanding Natural Beauty (AONB)',
+  'designated.conservationArea': 'Conservation Area',
+  'designated.greenBelt': 'Green Belt',
+  'designated.nationalPark': 'National Park',
+  'designated.nationalPark.broads': 'National Park - Broads',
+  'designated.SPA': 'Special Protection Area (SPA)',
+  'designated.WHS': 'UNESCO World Heritage Site or buffer zone',
+  flood: 'Flood Risk Zone',
+  'flood.zone.1': 'Flood Risk Zone 1 - Low risk',
+  'flood.zone.2': 'Flood Risk Zone 2 - Medium risk',
+  'flood.zone.3': 'Flood Risk Zone 3 - High risk',
+  listed: 'Listed Building',
+  locallyListed: 'Locally Listed Building',
+  monument: 'Site of a Scheduled Monument',
+  'nature.ASNW': 'Ancient Semi-Natural Woodland (ASNW)',
+  'nature.ramsarSite': 'Ramsar site',
+  'nature.SAC': 'Special Area of Conservation (SAC)',
+  'nature.SSSI': 'Site of Special Scientific Interest (SSSI)',
+  registeredPark: 'Historic Park or Garden',
+  'road.classified': 'Classified Road',
+  tpo: 'Tree Preservation Order (TPO) or zone',
+};
