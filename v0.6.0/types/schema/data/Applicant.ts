7ceabe243d585657c346d55b0d8972fbdb4ead20
jessicamcinchak <jessicamcinchak@users.noreply.github.com>
2024-05-07 10:34:20 +0000

    Add build files for v0.6.0

---
+++ v0.6.0/types/schema/data/Applicant.ts
@@ -0,0 +1,166 @@
+import {Date, Email} from '../../utils';
+import {User} from './User';
+
+/**
+ * @id #Applicant
+ * @description The user who completed the application either for themself or on behalf of someone else
+ */
+export type Applicant = BaseApplicant | Agent;
+
+/**
+ * @id #BaseApplicant
+ * @description Information about the user who completed the application for themself, or information about the person who the user applied on behalf of
+ */
+export interface BaseApplicant {
+  type: 'individual' | 'company' | 'charity' | 'public' | 'parishCouncil';
+  name: {
+    title?: string;
+    first: string;
+    last: string;
+  };
+  email: Email;
+  phone: {
+    primary: string;
+  };
+  company?: {
+    name: string;
+  };
+  address: UserAddress;
+  ownership?: Ownership;
+  siteContact: SiteContact;
+}
+
+/**
+ * @id #Ownership
+ * @description Information about the ownership certificate and property owners, if different than the applicant
+ */
+export interface Ownership {
+  interest?:
+    | 'owner'
+    | 'owner.sole'
+    | 'owner.co'
+    | 'tenant'
+    | 'occupier'
+    | 'other';
+  certificate?: 'a' | 'b' | 'c' | 'd';
+  /**
+   * @description Does the land have any agricultural tenants?
+   */
+  agriculturalTenants?: boolean;
+  /**
+   * @description Has requisite notice been given to all the known owners and agricultural tenants?
+   */
+  noticeGiven?: boolean;
+  /**
+   * @description Has a notice of the application been published in a newspaper circulating in the area where the land is situated?
+   */
+  noticePublished?: {
+    status: boolean;
+    date?: Date;
+    newspaperName?: string;
+  };
+  /**
+   * @description Do you know the names and addresses of all owners and agricultural tenants?
+   */
+  ownersKnown?: 'all' | 'some' | 'none';
+  owners?: Owners[];
+  /**
+   * @description Declaration of the accuracy of the ownership certificate, including reasonable steps taken to find all owners and publish notice
+   */
+  declaration?: {
+    accurate: true;
+  };
+}
+
+/**
+ * @id #Owners
+ * @description Names and addresses of all known owners and agricultural tenants, including confirmation or date of notice, or reason requisite notice has not been given if applicable
+ */
+export type Owners = OwnersNoticeGiven | OwnersNoNoticeGiven | OwnersNoticeDate;
+
+export interface BaseOwners {
+  name: string;
+  address: Address | string;
+  interest?: 'owner' | 'tenant' | 'occupier' | 'other';
+}
+
+// LDC requires `noticeGiven`, and `noNoticeReason` if false
+export interface OwnersNoticeGiven extends BaseOwners {
+  noticeGiven: true;
+}
+
+export interface OwnersNoNoticeGiven extends BaseOwners {
+  noticeGiven: false;
+  noNoticeReason: string;
+}
+
+// PP & LBC require `noticeDate`
+export interface OwnersNoticeDate extends BaseOwners {
+  noticeDate: Date;
+}
+
+/**
+ * @id #Agent
+ * @description Information about the agent or proxy who completed the application on behalf of someone else
+ */
+export interface Agent extends BaseApplicant {
+  agent: {
+    name: {
+      title?: string;
+      first: string;
+      last: string;
+    };
+    email: Email;
+    phone: {
+      primary: string;
+    };
+    company?: {
+      name: string;
+    };
+    address: Address;
+  };
+}
+
+/**
+ * @id #Address
+ * @description Address information for a person associated with this application not at the property address
+ */
+export interface Address {
+  line1: string;
+  line2?: string;
+  town: string;
+  county?: string;
+  postcode: string;
+  country?: string;
+}
+
+/**
+ * @id #UserAddress
+ * @description Address information for the applicant
+ */
+export type UserAddress = {sameAsSiteAddress: true} | UserAddressNotSameSite;
+
+/**
+ * @id #UserAddressNotSameSite
+ * @description Address information for an applicant with contact information that differs from the property address
+ */
+export interface UserAddressNotSameSite extends Address {
+  sameAsSiteAddress: false;
+}
+
+/**
+ * @id #SiteContact
+ * @description Contact information for the site visit
+ */
+export type SiteContact = {role: User['role']} | SiteContactOther;
+
+/**
+ * @id #SiteContactOther
+ * @description Contact information for the site visit when the SiteContact's role is 'other'
+ */
+export interface SiteContactOther {
+  role: 'other';
+  name: string;
+  email: string;
+  phone: string;
+}
---
+++ v0.6.0/types/schema/data/Proposal.ts
@@ -0,0 +1,225 @@
+import {GeoJSON} from 'geojson';
+import {
+  OpenSpaceTypes,
+  OpenSpaceDesignations,
+  ProtectedSpaceDesignations,
+} from '../../enums/Nature';
+import {ProjectTypes} from '../../enums/ProjectTypes';
+import {Area, Date} from '../../utils';
+import {Materials} from './shared';
+
+/**
+ * @id #Proposal
+ * @description Information about the proposed works and any changes to the property
+ */
+export type Proposal = BaseProposal | LondonProposal;
+
+export interface BaseProposal {
+  projectType: ProjectType[];
+  description: string;
+  /**
+   * @description Location plan boundary proposed by the user, commonly referred to as the red line boundary
+   */
+  boundary?: {
+    site: GeoJSON;
+    area: Area;
+  };
+  date?: ProposalDates;
+  /**
+   * @description Proposed materials, if applicable to projectType
+   */
+  materials?: Materials;
+  extend?: {
+    area: Area;
+  };
+  new?: {
+    area: Area;
+    count?: {
+      bathrooms?: number;
+      bedrooms?: number;
+      dwellings?: number;
+    };
+  };
+}
+
+/**
+ * @id #LondonProposal
+ * @description Proposal details for project sites within the Greater London Authority (GLA) area
+ */
+export interface LondonProposal extends BaseProposal {
+  schemeName?: string;
+  /**
+   * @description Proposed parking spaces
+   */
+  parking?: {
+    cars?: ProposedCount;
+    vans?: ProposedCount;
+    motorcycles?: ProposedCount;
+    cycles?: ProposedCount;
+    buses?: ProposedCount;
+    disabled?: ProposedCount;
+    carClub?: ProposedCount;
+    offStreet?: {residential: ProposedCount};
+    other?: ProposedCount;
+  };
+  /**
+   * @description Creating new buildings
+   */
+  newBuildings?: NewBuildingsOrStoreys;
+  /**
+   * @description Increasing the height of existing buildings
+   */
+  newStoreys?: NewBuildingsOrStoreys;
+  /**
+   * @description Project cost
+   */
+  cost?: {
+    projected: '2m' | '2mTo100m' | '100m';
+  };
+  /**
+   * @description Electric vehicle charing points
+   */
+  charging?: {
+    active: {count: number};
+    passive: {count: number};
+  };
+  /**
+   * @description Changes that result in the loss, gain, or change of use of natural spaces
+   */
+  nature?: {
+    openSpaces?: {
+      impact: 'loss' | 'gain' | 'change';
+      description: string;
+      type: OpenSpaceType;
+      designation: OpenSpaceDesignation;
+      access: 'restricted' | 'unrestricted';
+      area: {hectares: number};
+      /**
+       * @description Whether the open space change involves a land swap
+       */
+      swap: boolean;
+    }[];
+    protectedSpaces?: {
+      impact: 'loss' | 'gain' | 'change';
+      description: string;
+      designation: ProtectedSpaceDesignation;
+      access: 'restricted' | 'unrestricted';
+      area: {hectares: number};
+    }[];
+  };
+  /**
+   * @description Water management
+   */
+  water?: {
+    /**
+     * @description Internal residential water usage
+     */
+    usage: {litresPerPersonPerDay: number};
+    /**
+     * @description Whether the proposal includes rain water harvesting
+     */
+    rain: boolean;
+    /**
+     * @description Whether the proposal includes grey water re-use
+     */
+    grey: boolean;
+  };
+}
+
+type ProposedCount = {
+  count: number;
+  difference: number;
+};
+
+/**
+ * @id #NewBuildingsOrStoreys
+ * @description Details about creating new buildings or increasing the height of existing buildings
+ */
+export interface NewBuildingsOrStoreys {
+  count: number;
+  buildings?: {
+    height: {metres: number};
+    storeys: number;
+  }[];
+}
+
+/**
+ * @id #ProposalDates
+ * @description When the proposed works will start and be completed by, not required for all application types
+ */
+export interface ProposalDates {
+  start?: Date;
+  completion?: Date;
+}
+
+type ProjectTypeKeys = keyof typeof ProjectTypes;
+
+type GenericProjectType<TKey extends ProjectTypeKeys> = {
+  value: TKey;
+  description: (typeof ProjectTypes)[TKey];
+};
+
+type ProjectTypeMap = {
+  [K in ProjectTypeKeys]: GenericProjectType<K>;
+};
+
+/**
+ * @id #ProjectType
+ * @description Planning project types
+ */
+export type ProjectType = ProjectTypeMap[keyof ProjectTypeMap];
+
+type OpenSpaceTypeKeys = keyof typeof OpenSpaceTypes;
+
+type GenericOpenSpaceType<TKey extends OpenSpaceTypeKeys> = {
+  value: TKey;
+  description: (typeof OpenSpaceTypes)[TKey];
+};
+
+type OpenSpaceTypeMap = {
+  [K in OpenSpaceTypeKeys]: GenericOpenSpaceType<K>;
+};
+
+/**
+ * @id #OpenSpaceType
+ * @description Types of natural open spaces
+ */
+export type OpenSpaceType = OpenSpaceTypeMap[keyof OpenSpaceTypeMap];
+
+type OpenSpaceDesignationKeys = keyof typeof OpenSpaceDesignations;
+
+type GenericOpenSpaceDesignation<TKey extends OpenSpaceDesignationKeys> = {
+  value: TKey;
+  description: (typeof OpenSpaceDesignations)[TKey];
+};
+
+type OpenSpaceDesignationMap = {
+  [K in OpenSpaceDesignationKeys]: GenericOpenSpaceDesignation<K>;
+};
+
+/**
+ * @id #OpenSpaceDesignation
+ * @description Designations of natural open spaces
+ */
+export type OpenSpaceDesignation =
+  OpenSpaceDesignationMap[keyof OpenSpaceDesignationMap];
+
+type ProtectedSpaceDesignationKeys = keyof typeof ProtectedSpaceDesignations;
+
+type GenericProtectedSpaceDesignation<
+  TKey extends ProtectedSpaceDesignationKeys,
+> = {
+  value: TKey;
+  description: (typeof ProtectedSpaceDesignations)[TKey];
+};
+
+type ProtectedSpaceDesignationMap = {
+  [K in ProtectedSpaceDesignationKeys]: GenericProtectedSpaceDesignation<K>;
+};
+
+/**
+ * @id #ProtectedSpaceDesignation
+ * @description Designations of natural protected spaces
+ */
+export type ProtectedSpaceDesignation =
+  ProtectedSpaceDesignationMap[keyof ProtectedSpaceDesignationMap];
