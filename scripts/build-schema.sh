8a2bfc35f3d3ecf88fab07182f2ab0a28bf9cc6e
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-07-16 12:23:08 +0100

    refactor: Build scripts for multiple schemas

---
+++ scripts/build-schema.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Running build-schema script..."
+
+# Use environment variable VERSION, defaulting to "@next" if not set
+version="${VERSION:-@next}"
+
+echo "Version set to $version"
+
+dirs=("digitalPlanningApplication")
+types=("DigitalPlanningApplication")
+
+for i in "${!dirs[@]}"; do
+  dir=${dirs[$i]}
+  type=${types[$i]}
+
+  echo "Building schema for $type from directory /$dir"
+
+  pnpm ts-json-schema-generator --path "types/schemas/${dir}/*.ts" --out "schemas/${dir}.json" --type "$type" --id "$version" --no-top-ref
+done
