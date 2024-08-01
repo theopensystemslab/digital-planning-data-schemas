d1ac0423acd97b10694b1c4fb683c31759598635
DafyddLlyr <DafyddLlyr@users.noreply.github.com>
2024-08-01 14:37:53 +0000

    Add build files for @next

---
+++ @next/types/schemas/prototypeApplication/enums/FileType.ts
@@ -0,0 +1,607 @@
+/**
+ * @description Details of impact on access, roads, and rights of way
+ */
+type AccessRoadsRightsOfWayDetails = 'accessRoadsRightsOfWayDetails';
+
+/**
+ * @description Affordable housing statement
+ */
+type AffordableHousingStatement = 'affordableHousingStatement';
+
+/**
+ * @description Arboriculturist report
+ */
+type ArboriculturistReport = 'arboriculturistReport';
+
+/**
+ * @description Bank statement
+ */
+type BankStatement = 'bankStatement';
+
+/**
+ * @description Basement impact statement
+ */
+type BasementImpactStatement = 'basementImpactStatement';
+
+/**
+ * @description Bio-aerosol assessment
+ */
+type BioaerosolAssessment = 'bioaerosolAssessment';
+
+/**
+ * @description Birdstrike risk management plan
+ */
+type BirdstrikeRiskManagementPlan = 'birdstrikeRiskManagementPlan';
+
+/**
+ * @description Borehole or trial pit analysis
+ */
+type BoreholeOrTrialPitAnalysis = 'boreholeOrTrialPitAnalysis';
+
+/**
+ * @description Building control certificate
+ */
+type BuildingControlCertificate = 'buildingControlCertificate';
+
+/**
+ * @description Structural or building condition survey
+ */
+type ConditionSurvey = 'conditionSurvey';
+
+/**
+ * @description Construction invoice
+ */
+type ConstructionInvoice = 'constructionInvoice';
+
+/**
+ * @description Contamination report
+ */
+type ContaminationReport = 'contaminationReport';
+
+/**
+ * @description Council tax bill
+ */
+type CouncilTaxBill = 'councilTaxBill';
+
+/**
+ * @description Crime prevention strategy
+ */
+type CrimePreventionStrategy = 'crimePreventionStrategy';
+
+/**
+ * @description Design and Access Statement
+ */
+type DesignAndAccessStatement = 'designAndAccessStatement';
+
+/**
+ * @description Evidence for application fee exemption - disability
+ */
+type DisabilityExemptionEvidence = 'disabilityExemptionEvidence';
+
+/**
+ * @description Ecology report
+ */
+type EcologyReport = 'ecologyReport';
+
+/**
+ * @description Elevations - existing
+ */
+type ElevationsExisting = 'elevations.existing';
+
+/**
+ * @description Elevations - proposed
+ */
+type ElevationsProposed = 'elevations.proposed';
+
+/**
+ * @description Scheme for mitigation and monitoring of emissions (dust, odour and vibrations)
+ */
+type EmissionsMitigationAndMonitoringScheme =
+  'emissionsMitigationAndMonitoringScheme';
+
+/**
+ * @description Energy statement
+ */
+type EnergyStatement = 'energyStatement';
+
+/**
+ * @description Environmental Impact Assessment (EIA)
+ */
+type EnvironmentalImpactAssessment = 'environmentalImpactAssessment';
+
+/**
+ * @description External materials details
+ */
+type ExternalMaterialsDetails = 'externalMaterialsDetails';
+
+/**
+ * @description Fire safety report
+ */
+type FireSafetyReport = 'fireSafetyReport';
+
+/**
+ * @description Flood risk assessment (FRA)
+ */
+type FloodRiskAssessment = 'floodRiskAssessment';
+
+/**
+ * @description Floor plan - existing
+ */
+type FloorPlanExisting = 'floorPlan.existing';
+
+/**
+ * @description Floor plan - proposed
+ */
+type FloorPlanProposed = 'floorPlan.proposed';
+
+/**
+ * @description Foul drainage assessment
+ */
+type FoulDrainageAssessment = 'foulDrainageAssessment';
+
+/**
+ * @description Geodiversity assessment
+ */
+type GeodiversityAssessment = 'geodiversityAssessment';
+
+/**
+ * @description Plans showing the stretches of hedgerows to be removed
+ */
+type HedgerowsInformation = 'hedgerowsInformation';
+
+/**
+ * @description Evidence of the date of planting of the removed hedgerows
+ */
+type HedgerowsInformationPlantingDate = 'hedgerowsInformation.plantingDate';
+
+/**
+ * @description Heritage Statement
+ */
+type HeritageStatement = 'heritageStatement';
+
+/**
+ * @description Hydrological and hydrogeological assessment
+ */
+type HydrologicalAssessment = 'hydrologicalAssessment';
+
+/**
+ * @description Hydrology report
+ */
+type HydrologyReport = 'hydrologyReport';
+
+/**
+ * @description Internal elevations
+ */
+type InternalElevations = 'internalElevations';
+
+/**
+ * @description Internal sections
+ */
+type InternalSections = 'internalSections';
+
+/**
+ * @description Joiner's report
+ */
+type JoinersReport = 'joinersReport';
+
+/**
+ * @description Joinery section report
+ */
+type JoinerySections = 'joinerySections';
+
+/**
+ * @description Land contamination assessment
+ */
+type LandContaminationAssessment = 'landContaminationAssessment';
+
+/**
+ * @description Landscape and visual impact assessment (LVIA)
+ */
+type LandscapeAndVisualImpactAssessment = 'landscapeAndVisualImpactAssessment';
+
+/**
+ * @description Landscape strategy or landscape plan
+ */
+type LandscapeStrategy = 'landscapeStrategy';
+
+/**
+ * @description Lighting assessment
+ */
+type LightingAssessment = 'lightingAssessment';
+
+/**
+ * @description Details of litter, vermin and bird control
+ */
+type LitterVerminAndBirdControlDetails = 'litterVerminAndBirdControlDetails';
+
+/**
+ * @description Location plan
+ */
+type LocationPlan = 'locationPlan';
+
+/**
+ * @description Method statement
+ */
+type MethodStatement = 'methodStatement';
+
+/**
+ * @description Minerals and waste assessment
+ */
+type MineralsAndWasteAssessment = 'mineralsAndWasteAssessment';
+
+/**
+ * @description Information the authority considers necessary for the application
+ */
+type NecessaryInformation = 'necessaryInformation';
+
+/**
+ * @description New dwellings schedule
+ */
+type NewDwellingsSchedule = 'newDwellingsSchedule';
+
+/**
+ * @description Noise assessment
+ */
+type NoiseAssessment = 'noiseAssessment';
+
+/**
+ * @description Open space assessment
+ */
+type OpenSpaceAssessment = 'openSpaceAssessment';
+
+/**
+ * @description Other - document
+ */
+type OtherDocument = 'otherDocument';
+
+/**
+ * @description Other - drawing
+ */
+type OtherDrawing = 'otherDrawing';
+
+/**
+ * @description Other - evidence or correspondence
+ */
+type OtherEvidence = 'otherEvidence';
+
+/**
+ * @description Parking plan
+ */
+type ParkingPlan = 'parkingPlan';
+
+/**
+ * @description Photographs - existing
+ */
+type PhotographsExisting = 'photographs.existing';
+
+/**
+ * @description Photographs - proposed
+ */
+type PhotographsProposed = 'photographs.proposed';
+
+/**
+ * @description Planning statement
+ */
+type PlanningStatement = 'planningStatement';
+
+/**
+ * @description Recyclable waste storage details
+ */
+type RecycleWasteStorageDetails = 'recycleWasteStorageDetails';
+
+/**
+ * @description Information the applicant considers relevant to the application
+ */
+type RelevantInformation = 'relevantInformation';
+
+/**
+ * @description Residential units details
+ */
+type ResidentialUnitsDetails = 'residentialUnitsDetails';
+
+/**
+ * @description Roof plan - existing
+ */
+type RoofPlanExisting = 'roofPlan.existing';
+
+/**
+ * @description Roof plan - proposed
+ */
+type RoofPlanProposed = 'roofPlan.proposed';
+
+/**
+ * @description Sections - existing
+ */
+type SectionsExisting = 'sections.existing';
+
+/**
+ * @description Sections - proposed
+ */
+type SectionsProposed = 'sections.proposed';
+
+/**
+ * @description Site plan - existing
+ */
+type SitePlanExisting = 'sitePlan.existing';
+
+/**
+ * @description Site plan - proposed
+ */
+type SitePlanProposed = 'sitePlan.proposed';
+
+/**
+ * @description Sketch plan
+ */
+type SketchPlan = 'sketchPlan';
+
+/**
+ * @description Statement of community involvement
+ */
+type StatementOfCommunityInvolvement = 'statementOfCommunityInvolvement';
+
+/**
+ * @description Statutory declaration
+ */
+type StatutoryDeclaration = 'statutoryDeclaration';
+
+/**
+ * @description Details of storage treatment or disposal of waste
+ */
+type StorageTreatmentAndWasteDisposalDetails =
+  'storageTreatmentAndWasteDisposalDetails';
+
+/**
+ * @description Street scene drawing
+ */
+type StreetScene = 'streetScene';
+
+/**
+ * @description Subsidence report
+ */
+type SubsidenceReport = 'subsidenceReport';
+
+/**
+ * @description Sunlight and daylight report
+ */
+type SunlightAndDaylightReport = 'sunlightAndDaylightReport';
+
+/**
+ * @description Sustainability statement
+ */
+type SustainabilityStatement = 'sustainabilityStatement';
+
+/**
+ * @description Technical evidence
+ */
+type TechnicalEvidence = 'technicalEvidence';
+
+/**
+ * @description Technical specification
+ */
+type TechnicalSpecification = 'technicalSpecification';
+
+/**
+ * @description Tenancy agreement
+ */
+type TenancyAgreement = 'tenancyAgreement';
+
+/**
+ * @description Tenancy invoice
+ */
+type TenancyInvoice = 'tenancyInvoice';
+
+/**
+ * @description Town centre uses - Impact assessment
+ */
+type TownCentreImpactAssessment = 'townCentreImpactAssessment';
+
+/**
+ * @description Town centre uses - Sequential assessment
+ */
+type TownCentreSequentialAssessment = 'townCentreSequentialAssessment';
+
+/**
+ * @description Transport assessment
+ */
+type TransportAssessment = 'transportAssessment';
+
+/**
+ * @description Travel plan
+ */
+type TravelPlan = 'travelPlan';
+
+/**
+ * @description Location of trees and hedges
+ */
+type TreeAndHedgeLocation = 'treeAndHedgeLocation';
+
+/**
+ * @description Removed or pruned trees and hedges
+ */
+type TreeAndHedgeRemovedOrPruned = 'treeAndHedgeRemovedOrPruned';
+
+/**
+ * @description Tree canopy calculator
+ */
+type TreeCanopyCalculator = 'treeCanopyCalculator';
+
+/**
+ * @description Tree condition report
+ */
+type TreeConditionReport = 'treeConditionReport';
+
+/**
+ * @description Tree plan
+ */
+type TreePlan = 'treePlan';
+
+/**
+ * @description Trees report
+ */
+type TreesReport = 'treesReport';
+
+/**
+ * @description Unit plan - existing
+ */
+type UnitPlanExisting = 'unitPlan.existing';
+
+/**
+ * @description Unit plan - proposed
+ */
+type UnitPlanProposed = 'unitPlan.proposed';
+
+/**
+ * @description Use plan - existing
+ */
+type UsePlanExisting = 'usePlan.existing';
+
+/**
+ * @description Use plan - proposed
+ */
+type UsePlanProposed = 'usePlan.proposed';
+
+/**
+ * @description Utility bill
+ */
+type UtilityBill = 'utilityBill';
+
+/**
+ * @description Utilities statement
+ */
+type UtilitiesStatement = 'utilitiesStatement';
+
+/**
+ * @description Ventilation or extraction statement
+ */
+type VentilationStatement = 'ventilationStatement';
+
+/**
+ * @description Viability Appraisal
+ */
+type ViabilityAppraisal = 'viabilityAppraisal';
+
+/**
+ * @description Visualisations
+ */
+type Visualisations = 'visualisations';
+
+/**
+ * @description Waste and recycling strategy
+ */
+type WasteAndRecyclingStrategy = 'wasteAndRecyclingStrategy';
+
+/**
+ * @description Waste storage details
+ */
+type WasteStorageDetails = 'wasteStorageDetails';
+
+/**
+ * @description Water environment assessment
+ */
+type WaterEnvironmentAssessment = 'waterEnvironmentAssessment';
+
+/**
+ * @id #FileType
+ * @description Types of planning documents and drawings
+ */
+export type FileType =
+  | AccessRoadsRightsOfWayDetails
+  | AffordableHousingStatement
+  | ArboriculturistReport
+  | BankStatement
+  | BasementImpactStatement
+  | BioaerosolAssessment
+  | BirdstrikeRiskManagementPlan
+  | BoreholeOrTrialPitAnalysis
+  | BuildingControlCertificate
+  | ConditionSurvey
+  | ConstructionInvoice
+  | ContaminationReport
+  | CouncilTaxBill
+  | CrimePreventionStrategy
+  | DesignAndAccessStatement
+  | DisabilityExemptionEvidence
+  | EcologyReport
+  | ElevationsExisting
+  | ElevationsProposed
+  | EmissionsMitigationAndMonitoringScheme
+  | EnergyStatement
+  | EnvironmentalImpactAssessment
+  | ExternalMaterialsDetails
+  | FireSafetyReport
+  | FloodRiskAssessment
+  | FloorPlanExisting
+  | FloorPlanProposed
+  | FoulDrainageAssessment
+  | GeodiversityAssessment
+  | HedgerowsInformation
+  | HedgerowsInformationPlantingDate
+  | HeritageStatement
+  | HydrologicalAssessment
+  | HydrologyReport
+  | InternalElevations
+  | InternalSections
+  | JoinersReport
+  | JoinerySections
+  | LandContaminationAssessment
+  | LandscapeAndVisualImpactAssessment
+  | LandscapeStrategy
+  | LightingAssessment
+  | LitterVerminAndBirdControlDetails
+  | LocationPlan
+  | MethodStatement
+  | MineralsAndWasteAssessment
+  | NecessaryInformation
+  | NewDwellingsSchedule
+  | NoiseAssessment
+  | OpenSpaceAssessment
+  | OtherDocument
+  | OtherDrawing
+  | OtherEvidence
+  | ParkingPlan
+  | PhotographsExisting
+  | PhotographsProposed
+  | PlanningStatement
+  | RecycleWasteStorageDetails
+  | RelevantInformation
+  | ResidentialUnitsDetails
+  | RoofPlanExisting
+  | RoofPlanProposed
+  | SectionsExisting
+  | SectionsProposed
+  | SitePlanExisting
+  | SitePlanProposed
+  | SketchPlan
+  | StatementOfCommunityInvolvement
+  | StatutoryDeclaration
+  | StorageTreatmentAndWasteDisposalDetails
+  | StreetScene
+  | SubsidenceReport
+  | SunlightAndDaylightReport
+  | SustainabilityStatement
+  | TechnicalEvidence
+  | TechnicalSpecification
+  | TenancyAgreement
+  | TenancyInvoice
+  | TownCentreImpactAssessment
+  | TownCentreSequentialAssessment
+  | TransportAssessment
+  | TravelPlan
+  | TreeAndHedgeLocation
+  | TreeAndHedgeRemovedOrPruned
+  | TreeCanopyCalculator
+  | TreeConditionReport
+  | TreePlan
+  | TreesReport
+  | UnitPlanExisting
+  | UnitPlanProposed
+  | UsePlanExisting
+  | UsePlanProposed
+  | UtilityBill
+  | UtilitiesStatement
+  | VentilationStatement
+  | ViabilityAppraisal
+  | Visualisations
+  | WasteAndRecyclingStrategy
+  | WasteStorageDetails
+  | WaterEnvironmentAssessment;
