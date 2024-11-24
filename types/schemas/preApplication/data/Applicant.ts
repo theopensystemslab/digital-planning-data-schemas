d360944ded37872b02938e4de4e0c2ddfde2139a
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-11-24 14:53:54 +0100

    sync types to editor template service

---
+++ types/schemas/preApplication/data/Applicant.ts
@@ -0,0 +1,36 @@
+import {Email} from '../../../shared/utils';
+import {
+  Agent,
+  Ownership,
+  SiteContact,
+  UserAddress,
+} from '../../application/data/Applicant';
+
+/**
+ * @id #Applicant
+ * @description The user who completed the application either for themself or on behalf of someone else
+ */
+export type Applicant = BaseApplicant | Agent;
+
+/**
+ * @id #BaseApplicant
+ * @description Information about the user who completed the application for themself, or information about the person who the user applied on behalf of
+ */
+export interface BaseApplicant {
+  type: 'individual' | 'company' | 'charity' | 'public' | 'parishCouncil';
+  name: {
+    title?: string;
+    first: string;
+    last: string;
+  };
+  email: Email;
+  phone: {
+    primary: string;
+  };
+  company?: {
+    name: string;
+  };
+  address: UserAddress;
+  ownership?: Ownership;
+  siteContact: SiteContact;
+}
