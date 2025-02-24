794541fceff5f72c57f01e541fcae1c05181f0c5
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-02-24 22:16:39 +0100

    tweak types

--- types/shared/CommunityInfrastructureLevy.ts
+++ types/shared/CommunityInfrastructureLevy.ts
@@ -6,7 +6,7 @@ import {Area, Date} from './utils';
  */
 export type CommunityInfrastructureLevy = LiableForCIL | NotLiableForCIL;
 
-type LiableForCIL = {
+interface LiableForCIL extends BaseCIL {
   // Result checks in PlanX are heirarchical (first check if project qualifies for full exemption from CIL, then CIL relief, else plain "liable")
   result:
     | 'exempt.annexe'
@@ -15,14 +15,32 @@ type LiableForCIL = {
     | 'relief.charity'
     | 'relief.socialHousing'
     | 'liable';
-};
+}
 
-type NotLiableForCIL = {
+interface NotLiableForCIL extends BaseCIL {
   result: 'notLiable';
   declaration: true;
-};
+}
 
-type BaseCIL = {
+interface BaseCIL {
+  claim: {
+    /**
+     * @description Do you wish to claim an exemption for a residential annex or extension?
+     */
+    annexeOrExtensionExemption: boolean;
+    /**
+     * @description Do you wish to claim a self build exemption for a whole new home?
+     */
+    selfBuildExemption: boolean;
+    /**
+     * @description Is the site owned by a charity where the development will be wholly or mainly for charitable purposes, and the development will be either occupied by or under the control of a charitable institution?
+     */
+    charityRelief: boolean;
+    /**
+     * @description Does the proposed development include affordable housing which qualifies for mandatory or discretionary Social Housing relief?
+     */
+    socialHousingRelief: boolean;
+  };
   /**
    * @description Does the application seek to remove or vary conditions on an existing planning permission (Section 73)
    */
@@ -32,9 +50,9 @@ type BaseCIL = {
    */
   existingPermissionPrecedingCIL: boolean;
   /**
-   * @description Existing planning permission application reference number
+   * @description Planning application reference number for the existing permission
    */
-  existingPermissionReference: string;
+  existingPermissionReference?: string;
   /**
    * @description Does the application include creation of one or more new dwellings (including residential annexes) either through new build or conversion (except the conversion of a single dwelling house into two or more separate dwellings with no additional gross internal area created)?
    */
@@ -65,97 +83,79 @@ type BaseCIL = {
      */
     net: Area;
   };
-  existingBuildings: {
-    /**
-     * @description How many existing buildings on the site will be retained, demolished or partially demolished as part of the development proposed?
-     */
-    count: number;
-    /**
-     * @description Details of each individual existing building as applicable
-     */
-    details?: {
-      description: {
-        /**
-         * @description Description of existing building or part
-         */
-        existing: string;
-        /**
-         * @description Proposed use of retained gross internal area
-         */
-        proposed: string;
-      };
-      area: {
-        /**
-         * @description Gross internal area to be retained
-         */
-        retained: Area;
-        /**
-         * @description Gross internal area to be demolished
-         */
-        loss: Area;
-      };
+  /**
+   * @description How many existing buildings on the site will be retained, demolished or partially demolished as part of the development proposed?
+   */
+  existingBuildings?: {
+    description: {
       /**
-       * @description Was the building or part of the building occupied for its lawful use for 6 continuous months of the 36 previous months (excluding temporary permissions)?
+       * @description Description of existing building or part
        */
-      continuousOccupation: boolean;
+      existing: string;
       /**
-       * @description Is the building still occupied for its lawful use?
+       * @description Proposed use of retained gross internal area
        */
-      stillInUse: boolean;
+      proposed: string;
+    };
+    area: {
       /**
-       * @description When was the building last occupied for its lawful use?
+       * @description Gross internal area to be retained
        */
-      lastOccupation: Date;
-    }[];
-  };
-  unoccupiedBuildings: {
-    /**
-     * @description Are there existing buildings on the property into which people do not usually go or that have been granted temporary planning permission?
-     */
-    count: number;
-    /**
-     * @description Details of each individual unoccupied building as applicable
-     */
-    details?: {
-      description: {
-        /**
-         * @description Description of unoccupied building or part
-         */
-        existing: string;
-        /**
-         * @description Proposed use of retained gross internal area
-         */
-        proposed: string;
-      };
-      area: {
-        /**
-         * @description Gross internal area to be retained
-         */
-        retained: Area;
-        /**
-         * @description Gross internal area to be demolished
-         */
-        loss: Area;
-      };
-    }[];
-  };
-  newMezzanine: {
-    /**
-     * @description Will the project create a new mezzanine floor within an existing building?
-     */
-    applicable: boolean;
-    /**
-     * @description Details of each new mezzaine floor with an existing building as applicable
-     */
-    details?: {
+      retained: Area;
       /**
-       * @description Description of the use of the new mezzanine
+       * @description Gross internal area to be demolished
        */
-      description: string;
+      loss: Area;
+    };
+    /**
+     * @description Was the building or part of the building occupied for its lawful use for 6 continuous months of the 36 previous months (excluding temporary permissions)?
+     */
+    continuousOccupation: boolean;
+    /**
+     * @description Is the building still occupied for its lawful use?
+     */
+    stillInUse: boolean;
+    /**
+     * @description When was the building last occupied for its lawful use?
+     */
+    lastOccupation: Date;
+  }[];
+  /**
+   * @description Are there existing buildings on the property into which people do not usually go or that have been granted temporary planning permission?
+   */
+  unoccupiedBuildings?: {
+    description: {
       /**
-       * @description Proposed floorspace of the new mezzanine
+       * @description Description of unoccupied building or part
        */
-      area: Area;
-    }[];
-  };
-};
+      existing: string;
+      /**
+       * @description Proposed use of retained gross internal area
+       */
+      proposed: string;
+    };
+    area: {
+      /**
+       * @description Gross internal area to be retained
+       */
+      retained: Area;
+      /**
+       * @description Gross internal area to be demolished
+       */
+      loss: Area;
+    };
+  }[];
+  /**
+   * @description Will the project create a new mezzanine floor within an existing building?
+   */
+  newMezzanine?: {
+    /**
+     * @description Description of the use of the new mezzanine
+     */
+    description: string;
+    /**
+     * @description Proposed floorspace of the new mezzanine
+     */
+    area: Area;
+  }[];
+}
